f780f05307e000177ed077ce5d012b06912f805b
Lucas Lavandeira <lucaslavandeira@gmail.com>
2023-02-13 11:43:01 -0300

    Move database url to environment variable

--- src/repositories/database.py
+++ src/repositories/database.py
@@ -2,12 +2,14 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 
+from src import config
+
 Base = declarative_base()
 
 
 class SessionFactory:
     def __init__(self) -> None:
-        self.engine = create_engine("postgresql://postgres:postgres@localhost/postgres")
+        self.engine = create_engine(config.DATABASE_URL)
         Base.metadata.create_all(self.engine)
         self.sessionmaker = sessionmaker(bind=self.engine)
 
